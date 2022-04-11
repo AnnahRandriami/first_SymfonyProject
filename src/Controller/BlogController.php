f0f8595917ca9e55fb610369c26290444a39f57d
AnnahRandriami <niniannah@gmail.com>
2022-04-11 14:55:57 +0200

    add create/uptade article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,18 +4,30 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use Symfony\Component\HttpFoundation\Request;
+
+use Doctrine\Persistence\ObjectManager;
+
 use Symfony\Component\Routing\Annotation\Route;
+
 use App\Entity\Article;
+use App\Repository\ArticleRepository;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+
+
 
 class BlogController extends AbstractController
 {
     /**
      * @Route("/blog", name="blog")
      */
-    public function index()
+    public function index(ArticleRepository $repo) //injection de dependance
     {
-        $repo = $this->getDoctrine()->getRepository(Article::class);
-
         $articles = $repo->findAll();
 
         return $this->render('blog/index.html.twig', [
@@ -33,12 +45,54 @@ class BlogController extends AbstractController
             'age' => 12,
         ]);
     }
+
+    /**
+        * @Route("/blog/new", name="blog_create")
+        * @Route("/blog/{id}/edit", name="blog_edit")
+        */
+       public function form(Article $article = null, Request $request, ObjectManager $manager){
+
+        if(!$article){
+            $article = new Article();
+        }
+
+    
+
+        $form = $this->createFormBuilder($article)
+                      ->add('title')
+                      ->add('content')
+                      ->add('image')
+                      ->getForm();
+
+    $form->handleRequest($request);
+
+   if($form->isSubmitted() && $form->isValid()){
+      if(!$article->getId()){
+          $article->setCreatedAt(new \DateTimeImmutable());
+
+      }
+    
+
+       $manager->persist($article);
+       $manager->flush();
+
+       return $this->redirectToRoute('blog_show', ['id' => $article->getId()]);
+   }
+
+           return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView(),
+            'editMode' =>$article->getId() !== null
+          
+           ]);
+
+           
+        
+       }
+
    /**
      * @Route("/blog/{id}", name="blog_show")
      */
-    public function show($id){
-        $repo = $this->getDoctrine()->getRepository(Article::class);
-        $article = $repo->find($id);
+    public function show(Article $article){
         return $this->render('blog/show.html.twig' , [
             'article' => $article
         ]);
