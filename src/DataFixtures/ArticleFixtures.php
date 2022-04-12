2b146bac5a843581fc367e3888896e76c63533eb
AnnahRandriami <niniannah@gmail.com>
2022-04-12 11:11:38 +0200

    add category/comments

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,17 +12,44 @@ class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        //ajout de 10 article dans ma table artile dans la base de doonée
-        for ($i = 1; $i <= 10; $i++) {
-            $article = new Article();
-            $article->setTitle("Titre de l'artcile n) $i")
-            ->setContent("<p> Le contenu de l'article n°$i </p>")
-            ->setImage("http://placehold.it/350x150")
-            ->setCreatedAt(new \DateTimeImmutable());
-            $manager->persist($article); //ajout dans la base de donnée
-      }
 
-        $manager->flush(); //mise en place des manip
+        $faker = \Faker\Factory::create('fr_FR');
+
+        //créer trois category fake
+        for ($i = 0; $i <= 3; $i++) {
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+
+          
+            for ($j = 1; $j <= mt_rand(4, 6); $j++) {
+                $article = new Article();
+                $content = '<p>' . join($faker->paragraphs(5) , '</p><p>') . '</p>';
+                $article->setTitle($faker->sentence())
+                    ->setContent($content)
+                    ->setImage($faker->imageUrl())
+                    ->setCreatedAt(new \DateTimeImmutable())
+                    ->setCategory($category);
+
+                $manager->persist($article); //ajout dans la base de donnée
+
+                for ($k = 1; $k <= mt_rand(4, 6); $k++) {
+                    $comment = new Comment();
+                    $content = '<p>' . join($faker->paragraphs(2) , '</p><p>') . '</p>';
+                    $comment->setAuthor($faker->name)
+                        ->setContent($content)
+                        ->setCreatedAt(new \DateTimeImmutable())
+                        ->setArticle($article);
+
+                    $manager->persist($comment);
+                }
+            }
+        }
+
+
+
+        $manager->flush();
     }
 }
- 
